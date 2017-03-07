bbece799dbc815d0c9eba638a2a9aa75a33f16d3
mmuchai <muchaimercyw@gmail.com>
2017-03-07 12:36:14 +0300

    Prime numbers Real Final Copy

--- prime_numbers_tests.py
+++ prime_numbers_tests.py
@@ -12,18 +12,6 @@ class TestSolution (unittest.TestCase):
 		self.assertEqual (is_prime(3), 3)
 	def test_returns_list_of_primes(self):
 		self.assertTrue(list_of_prime_numbers(10), [3,5,7])
-	# def test_list_of_prime_numbers(self):
-	# 	self.assertTrue (list_of_prime_numbers(10), [3, 5, 7])
-	# def test_cannot_take_negative(self):
-	# 	self.assertFalse(list_of_prime_numbers (-5), "Value should not be negative")
-	# def test_must_be_integer(self):
-	# 	self.assertTrue(list_of_prime_numbers ("cats"), "Can only take positive integer")
-	# 	"""Fot string or boolean, raise and error"""
-	# def test_list_prime_numbers_from_zero_to_n_inclusive(self):
-	# 	self.assertTrue(list_of_prime_numbers (11), [3,5,7,11])
-	# def test_should_not_be_below_two(self):
-	# 	self.assertFalse (list_of_prime_numbers (1),)
-	# def test_not_even_number(self):
-	# 	self.assertFalse(list_of_prime_numbers (4))
+		
 if __name__ == '__main__':
 	unittest.main()
